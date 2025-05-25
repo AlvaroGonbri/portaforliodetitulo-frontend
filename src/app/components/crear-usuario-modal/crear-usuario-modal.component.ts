304ff261aa42335b21c4de64357d9bb487f43bea
SaravClaudio <cla.varas@duocuc.cl>
2025-05-25 19:26:46 -0400

    Visualizacion terminada

--- src/app/components/crear-usuario-modal/crear-usuario-modal.component.ts
+++ src/app/components/crear-usuario-modal/crear-usuario-modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { ModalController, ToastController } from '@ionic/angular';
 import { APIService } from '../../services/API/api.service';
 
 @Component({
@@ -14,43 +14,50 @@ export class CrearUsuarioModalComponent implements OnInit {
 
   constructor(
     private modalController: ModalController,
-    private apiService: APIService
+    private apiService: APIService,
+    private toastController: ToastController
   ) {}
 
-ngOnInit() {
-  // Si no se pasa un usuario, inicializa uno nuevo
-  if (!this.usuario) {
-    this.usuario = {
-      username: '',
-      email: '',
-      first_name: '',
-      last_name: '',
-      password: '',
-      cargo: '',
-      grupoSeleccionado: null
-    };
+  ngOnInit() {
+    // Si no se pasa un usuario, inicializa uno nuevo
+    if (!this.usuario) {
+      this.usuario = {
+        username: '',
+        email: '',
+        first_name: '',
+        last_name: '',
+        password: '',
+        cargo: '',
+        grupoSeleccionado: null
+      };
+    }
+    // Si el usuario tiene grupos, inicializa el grupo seleccionado
+    if (this.usuario.groups && this.usuario.groups.length > 0) {
+      this.usuario.grupoSeleccionado = this.usuario.groups[0].id;
+    } else {
+      this.usuario.grupoSeleccionado = null;
+    }
+    // Si no se pasan grupos, carga los grupos
+    if (!this.grupos || this.grupos.length === 0) {
+      this.apiService.getgroups().subscribe({
+        next: (grupos) => { this.grupos = grupos; },
+        error: (err) => { console.error('Error al obtener grupos', err); }
+      });
+    }
   }
-  // Si el usuario tiene grupos, inicializa el grupo seleccionado
-  // Si groups es un array de objetos {id, name}
-  if (this.usuario.groups && this.usuario.groups.length > 0) {
-    // Si es edición y groups es array de objetos
-    this.usuario.grupoSeleccionado = this.usuario.groups[0].id;
-    // Si fuera array de IDs, sería: this.usuario.grupoSeleccionado = this.usuario.groups[0];
-  } else {
-    this.usuario.grupoSeleccionado = null;
-  }
-  // Si no se pasan grupos, carga los grupos
-  if (!this.grupos || this.grupos.length === 0) {
-    this.apiService.getgroups().subscribe({
-      next: (grupos) => { this.grupos = grupos; },
-      error: (err) => { console.error('Error al obtener grupos', err); }
-    });
-  }
-}
-
 
   dismiss() {
     this.modalController.dismiss();
+  }
+
+  async mostrarToastExito() {
+    const toast = await this.toastController.create({
+      message: 'Usuario guardado con éxito',
+      duration: 2000,
+      color: 'success',
+      position: 'top'
+    });
+    await toast.present();
   }
 
   crearUsuario() {
@@ -60,12 +67,13 @@ ngOnInit() {
       profile: { cargo: this.usuario.cargo }
     };
     if (!usuarioParaEnviar.password) {
-  delete usuarioParaEnviar.password;
-}
+      delete usuarioParaEnviar.password;
+    }
     // Si el usuario tiene id, es una edición
     if (this.usuario.id) {
       this.apiService.editarUsuario(this.usuario.id, usuarioParaEnviar).subscribe({
-        next: (respuesta) => {
+        next: async (respuesta) => {
+          await this.mostrarToastExito();
           this.modalController.dismiss(usuarioParaEnviar);
         },
         error: (err) => {
@@ -75,7 +83,8 @@ ngOnInit() {
     } else {
       // Si no tiene id, es una creación
       this.apiService.crearUsuario(usuarioParaEnviar).subscribe({
-        next: (respuesta) => {
+        next: async (respuesta) => {
+          await this.mostrarToastExito();
           this.modalController.dismiss(usuarioParaEnviar);
         },
         error: (err) => {
